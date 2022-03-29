5faf945d9ae7f829ca07ed902e612facbbd64662
kekevi <kekevi@gitlab.com>
2022-03-29 18:36:28 -0400

    Identifier has optional constructor with init decl

--- compiler-checkpoint2/src/miniJava/AbstractSyntaxTrees/Identifier.java
+++ compiler-checkpoint2/src/miniJava/AbstractSyntaxTrees/Identifier.java
@@ -8,10 +8,16 @@ package miniJava.AbstractSyntaxTrees;
 import miniJava.SyntacticAnalyzer.Token;
 
 public class Identifier extends Terminal {
+  public Declaration decl;
 
   public Identifier (Token t) {
     super (t);
   }
+
+  public Identifier (Token t, Declaration decl) {
+    this(t);
+    this.decl = decl;
+  } 
 
   public <A,R> R visit(Visitor<A,R> v, A o) {
       return v.visitIdentifier(this, o);
